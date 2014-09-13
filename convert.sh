14ae929cd8b3cd458d08bcadf951f255d8df6ad2
Heikki Ylönen <heikki74@gmail.com>
2014-09-13 08:53:03 +0300

    Another try for argument passing.

--- convert.sh
+++ convert.sh
@@ -2,16 +2,20 @@
 
 rm converted/**/* 2&>/dev/null
 
-for FILE in ${1:-mpo/*.MPO}
+[ "$#" -gt 0 ] || set -- mpo/*.MPO
+
+for FILE in $@
 do
 
 	# OUTPUT=`basename -s '.MPO' $FILE`.jpg
 	OUTPUT=`exiftool -CreateDate -d "%Y-%m-%dT%H-%M-%S" -S -t $FILE`.jpg
 
+	echo `basename $FILE` -\> $OUTPUT
+
 	LEFT=converted/left/$OUTPUT
 	RIGHT=converted/right/$OUTPUT
 
-	exiftool -trailer:all= $FILE -o $LEFT
+	exiftool -trailer:all= $FILE -o $LEFT > /dev/null
 	exiftool $FILE -mpimage2 -b > $RIGHT
 
 	convert $RIGHT $LEFT +append converted/right-left/$OUTPUT
