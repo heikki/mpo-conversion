407590c2f3e77be46202cd866a557ace9ee5191e
Heikki Ylönen <heikki74@gmail.com>
2014-10-08 02:33:09 +0300

    Tweaks.

--- convert.sh
+++ convert.sh
@@ -7,17 +7,17 @@ rm converted/**/* 2&>/dev/null
 for FILE in $@
 do
 
-	# OUTPUT=`basename -s '.MPO' $FILE`.jpg
-	OUTPUT=`exiftool -CreateDate -d "%Y-%m-%dT%H-%M-%S" -S -t $FILE`.jpg
+	OUTPUT=`basename -s '.MPO' $FILE | tr "[:upper:]" "[:lower:]"`
+	# OUTPUT=`exiftool -CreateDate -d "%Y-%m-%dT%H-%M-%S" -S -t $FILE`.jpg
 
-	echo `basename $FILE` -\> $OUTPUT
+	echo `basename $FILE` -\> $OUTPUT.jpg
 
-	LEFT=converted/left/$OUTPUT
-	RIGHT=converted/right/$OUTPUT
+	LEFT=converted/separate/$OUTPUT-left.jpg
+	RIGHT=converted/separate/$OUTPUT-right.jpg
 
 	exiftool -trailer:all= $FILE -o $LEFT > /dev/null
 	exiftool $FILE -mpimage2 -b > $RIGHT
 
-	convert $RIGHT $LEFT +append converted/right-left/$OUTPUT
+	convert $RIGHT $LEFT +append converted/cross-eye/$OUTPUT.jpg
 
 done
