ea05d5549b76a5a057cf5f9f9c22d15a27b7dbc0
Heikki Ylönen <heikki74@gmail.com>
2015-03-16 19:28:05 -0400

    Simplify

--- combine.sh
+++ combine.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 
 convert \
-	'converted/test-right.jpg' \
-	'converted/test-left.jpg' \
-	+append \
-	converted/test-a.jpg
+	converted/test-right.jpg \
+	converted/test-left.jpg \
+	+append converted/test-a.jpg
 
 convert \
-	'converted/test-right.jpg' \
-	'converted/test-left.jpg[+0+9]' \
-	+append \
-	-crop '+0-9' \
-	converted/test-b.jpg
+	\( converted/test-right.jpg -crop '+0-9' \) \
+	\( converted/test-left.jpg  -crop '+0+9' \) \
+	+append converted/test-b.jpg
